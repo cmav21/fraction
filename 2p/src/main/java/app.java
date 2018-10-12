e2b36f2a5fdca6114dce336de48972be12f6f396
Cesar <cesar9mv@gmail.com>
2018-10-11 23:08:35 -0500

    AddToPractica

--- 2p/src/main/java/app.java
+++ 2p/src/main/java/app.java
@@ -5,7 +5,14 @@ public class app {
     public static void main(String[] args) {
         fraction f = new fraction(9,3);
         fraction f2 = new fraction(5,3);
-        fraction result = f.simplificar(f.dividir(f,f2));
-        System.out.println(result.getN() + "/" + result.getD());
+        fraction resultD = f.simplificar(f.dividir(f,f2));
+        fraction resultS = f.simplificar(f.sumar(f,f2));
+        fraction resultR = f.simplificar(f.restar(f,f2));
+        fraction resultM = f.simplificar(f.multiplicar(f,f2));
+        System.out.println(resultD.getN() + "/" + resultD.getD());
+        System.out.println(resultS.getN() + "/" + resultS.getD());
+        System.out.println(resultR.getN() + "/" + resultR.getD());
+        System.out.println(resultM.getN() + "/" + resultM.getD());
+        
     }
 }
